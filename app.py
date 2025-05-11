fc36a59cbf3a24d4df65386b07b929907c94095e
Nabiha Rajani <156829737+nbh-rjn@users.noreply.github.com>
2025-05-11 14:13:47 +0500

    Update app.py to run on 0.0.0.0

--- app.py
+++ app.py
@@ -163,5 +163,5 @@ def upload_file(current_user):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
